d5a23df329415af30318210b00ab754fb4ca5206
Vladimyr23 <vladimyr.yes@gmail.com>
2023-06-05 16:40:10 +0100

    strip matching FGR and RANSAC added

--- PointCloudGUI/VisualizePCD.py
+++ PointCloudGUI/VisualizePCD.py
@@ -4,6 +4,7 @@ from threading import Thread
 import matplotlib.pyplot as plt
 import copy
 import argparse
+import time
 
 
 class VisualizePCD(Thread):
@@ -87,7 +88,69 @@ class VisualizePCD(Thread):
         # o3d.visualization.draw([source_temp, target_temp])
         #--------------------ROBUST ICP REGISTRATION END
 
-        #FGR REGISTRATION START--------------------
+        # #FGR REGISTRATION START--------------------
+        # parser = argparse.ArgumentParser(
+        #     'Global point cloud registration example with RANSAC')
+        # parser.add_argument('src',
+        #                     type=str,
+        #                     default=source,
+        #                     nargs='?',
+        #                     help='path to src point cloud')
+        # parser.add_argument('dst',
+        #                     type=str,
+        #                     default=target,
+        #                     nargs='?',
+        #                     help='path to dst point cloud')
+        # parser.add_argument('--voxel_size',
+        #                     type=float,
+        #                     default=0.05,
+        #                     help='voxel size in meter used to downsample inputs')
+        # parser.add_argument(
+        #     '--distance_multiplier',
+        #     type=float,
+        #     default=1.5,
+        #     help='multipler used to compute distance threshold'
+        #          'between correspondences.'
+        #          'Threshold is computed by voxel_size * distance_multiplier.')
+        # parser.add_argument('--max_iterations',
+        #                     type=int,
+        #                     default=64,
+        #                     help='number of max FGR iterations')
+        # parser.add_argument(
+        #     '--max_tuples',
+        #     type=int,
+        #     default=1000,
+        #     help='max number of accepted tuples for correpsondence filtering')
+        #
+        # args = parser.parse_args()
+        #
+        # voxel_size = args.voxel_size
+        # distance_threshold = args.distance_multiplier * voxel_size
+        # print("Distance threshold: ", distance_threshold)
+        #
+        # o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Debug)
+        # print('Reading inputs')
+        # src = source
+        # dst = target
+        #
+        # print('Downsampling inputs')
+        # src_down, src_fpfh = self.preprocess_point_cloud(src, voxel_size)
+        # dst_down, dst_fpfh = self.preprocess_point_cloud(dst, voxel_size)
+        # start = time.time()
+        # print('FGR Started', start)
+        # result = o3d.pipelines.registration.registration_fgr_based_on_feature_matching(
+        #     src_down, dst_down, src_fpfh, dst_fpfh,
+        #     o3d.pipelines.registration.FastGlobalRegistrationOption(
+        #         maximum_correspondence_distance=distance_threshold,
+        #         iteration_number=args.max_iterations,
+        #         maximum_tuple_count=args.max_tuples))
+        # print('FGR Finished', time.time(), "Global registration took %.3f sec.\n" % (time.time() - start))
+        # src.paint_uniform_color([1, 0, 0])
+        # dst.paint_uniform_color([0, 1, 0])
+        # o3d.visualization.draw([src.transform(result.transformation), dst])
+        # #--------------------FGR REGISTRATION END
+
+        # RANSAC REGISTRATION START--------------------
         parser = argparse.ArgumentParser(
             'Global point cloud registration example with RANSAC')
         parser.add_argument('src',
@@ -113,13 +176,16 @@ class VisualizePCD(Thread):
                  'Threshold is computed by voxel_size * distance_multiplier.')
         parser.add_argument('--max_iterations',
                             type=int,
-                            default=64,
-                            help='number of max FGR iterations')
+                            default=1000000,
+                            help='number of max RANSAC iterations')
+        parser.add_argument('--confidence',
+                            type=float,
+                            default=0.999,
+                            help='RANSAC confidence')
         parser.add_argument(
-            '--max_tuples',
-            type=int,
-            default=1000,
-            help='max number of accepted tuples for correpsondence filtering')
+            '--mutual_filter',
+            action='store_true',
+            help='whether to use mutual filter for putative correspondences')
 
         args = parser.parse_args()
 
@@ -128,28 +194,39 @@ class VisualizePCD(Thread):
         print("Distance threshold: ", distance_threshold)
 
         o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Debug)
-        print('Reading inputs')
-        src = source
-        dst = target
+        # print('Reading inputs')
+        # src = source
+        # dst = target
 
         print('Downsampling inputs')
-        src_down, src_fpfh = self.preprocess_point_cloud(src, voxel_size)
-        dst_down, dst_fpfh = self.preprocess_point_cloud(dst, voxel_size)
+        src_down, src_fpfh = self.preprocess_point_cloud(source, voxel_size)
+        dst_down, dst_fpfh = self.preprocess_point_cloud(target, voxel_size)
 
-        print('Running FGR')
-        result = o3d.pipelines.registration.registration_fgr_based_on_feature_matching(
+        start = time.time()
+        print('RANSAC Started', start)
+        print('Running RANSAC')
+        result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
             src_down, dst_down, src_fpfh, dst_fpfh,
-            o3d.pipelines.registration.FastGlobalRegistrationOption(
-                maximum_correspondence_distance=distance_threshold,
-                iteration_number=args.max_iterations,
-                maximum_tuple_count=args.max_tuples))
+            mutual_filter=args.mutual_filter,
+            max_correspondence_distance=distance_threshold,
+            estimation_method=o3d.pipelines.registration.
+            TransformationEstimationPointToPoint(False),
+            ransac_n=3,
+            checkers=[
+                o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
+                    0.9),
+                o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                    distance_threshold)
+            ],
+            criteria=o3d.pipelines.registration.RANSACConvergenceCriteria(
+                args.max_iterations, args.confidence))
+        print('RANSAC Finished', time.time(),
+              "Global registration took %.3f sec.\n" % (time.time() - start))
 
-        src.paint_uniform_color([1, 0, 0])
-        dst.paint_uniform_color([0, 1, 0])
-        o3d.visualization.draw([src.transform(result.transformation), dst])
-        #--------------------FGR REGISTRATION END
-
-
+        source.paint_uniform_color([1, 0, 0])
+        target.paint_uniform_color([0, 1, 0])
+        o3d.visualization.draw([source.transform(result.transformation), target])
+        # --------------------RANSAC REGISTRATION END
 
     def pick_points(self,pcd):
         print("")
