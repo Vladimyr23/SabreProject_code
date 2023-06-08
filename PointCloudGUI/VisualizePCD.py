66d07e70bd82e9df89580fd3738dc0e718385c98
Vladimyr23 <vladimyr.yes@gmail.com>
2023-06-08 15:36:42 +0100

    RANSAC ICP local refinement added

--- PointCloudGUI/VisualizePCD.py
+++ PointCloudGUI/VisualizePCD.py
@@ -201,119 +201,129 @@ class VisualizePCD():
         # o3d.visualization.draw([src.transform(result.transformation), dst])
         # #--------------------FGR REGISTRATION END
 
-        # # RANSAC REGISTRATION START--------------------
-        # parser = argparse.ArgumentParser(
-        #     'Global point cloud registration example with RANSAC')
-        # parser.add_argument('src',
-        #                     type=str,
-        #                     default=source,
-        #                     nargs='?',
-        #                     help='path to src point cloud')
-        # parser.add_argument('dst',
-        #                     type=str,
-        #                     default=target,
-        #                     nargs='?',
-        #                     help='path to dst point cloud')
-        # parser.add_argument('--voxel_size',
-        #                     type=float,
-        #                     default=0.05,
-        #                     help='voxel size in meter used to down sample inputs')
-        # parser.add_argument(
-        #     '--distance_multiplier',
-        #     type=float,
-        #     default=1.5,
-        #     help='multiplier used to compute distance threshold'
-        #          'between correspondences.'
-        #          'Threshold is computed by voxel_size * distance_multiplier.')
-        # parser.add_argument('--max_iterations',
-        #                     type=int,
-        #                     default=1000000,
-        #                     help='number of max RANSAC iterations')
-        # parser.add_argument('--confidence',
-        #                     type=float,
-        #                     default=0.999,
-        #                     help='RANSAC confidence')
-        # parser.add_argument(
-        #     '--mutual_filter',
-        #     action='store_true',
-        #     help='whether to use mutual filter for putative correspondences')
-        #
-        # args = parser.parse_args()
-        #
-        # voxel_size = args.voxel_size
-        # distance_threshold = args.distance_multiplier * voxel_size
-        # print("Distance threshold: ", distance_threshold)
-        #
-        # o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Debug)
-        # # print('Reading inputs')
-        # # src = source
-        # # dst = target
-        #
-        # print('Down sampling inputs')
-        # src_down, src_fpfh = self.preprocess_point_cloud(source, voxel_size)
-        # dst_down, dst_fpfh = self.preprocess_point_cloud(target, voxel_size)
-        #
-        # start = time.time()
-        # print('RANSAC Started', time.localtime(start))
-        # print('Running RANSAC')
-        # result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
-        #     src_down, dst_down, src_fpfh, dst_fpfh,
-        #     mutual_filter=args.mutual_filter,
-        #     max_correspondence_distance=distance_threshold,
-        #     estimation_method=o3d.pipelines.registration.
-        #     TransformationEstimationPointToPoint(False),
-        #     ransac_n=3,
-        #     checkers=[
-        #         o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
-        #             0.9),
-        #         o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
-        #             distance_threshold)
-        #     ],
-        #     criteria=o3d.pipelines.registration.RANSACConvergenceCriteria(
-        #         args.max_iterations, args.confidence))
-        # print('RANSAC Finished', time.localtime(time.time()),
-        #       "Global registration took %.3f sec.\n" % (time.time() - start))
-        #
-        # # source.paint_uniform_color([1, 0, 0])
-        # # target.paint_uniform_color([0, 1, 0])
-        # o3d.visualization.draw([source.transform(result.transformation), target])
-        # # --------------------RANSAC REGISTRATION END
+        # RANSAC REGISTRATION START--------------------
+        parser = argparse.ArgumentParser(
+            'Global point cloud registration example with RANSAC')
+        parser.add_argument('src',
+                            type=str,
+                            default=sour,
+                            nargs='?',
+                            help='path to src point cloud')
+        parser.add_argument('dst',
+                            type=str,
+                            default=targ,
+                            nargs='?',
+                            help='path to dst point cloud')
+        parser.add_argument('--voxel_size',
+                            type=float,
+                            default=0.05,
+                            help='voxel size in meter used to down sample inputs')
+        parser.add_argument(
+            '--distance_multiplier',
+            type=float,
+            default=1.5,
+            help='multiplier used to compute distance threshold'
+                 'between correspondences.'
+                 'Threshold is computed by voxel_size * distance_multiplier.')
+        parser.add_argument('--max_iterations',
+                            type=int,
+                            default=1000000,
+                            help='number of max RANSAC iterations')
+        parser.add_argument('--confidence',
+                            type=float,
+                            default=0.999,
+                            help='RANSAC confidence')
+        parser.add_argument(
+            '--mutual_filter',
+            action='store_true',
+            help='whether to use mutual filter for putative correspondences')
 
-        # Multiway REGISTRATION START--------------------
-        voxel_size = 0.02
-        pcds_down = []
-        source_down = sour.voxel_down_sample(voxel_size=voxel_size)
-        pcds_down.append(source_down)
-        target_down = targ.voxel_down_sample(voxel_size=voxel_size)
-        pcds_down.append(target_down)
-        print("Full registration ...")
-        max_correspondence_distance_coarse = voxel_size * 15
-        max_correspondence_distance_fine = voxel_size * 1.5
-        with o3d.utility.VerbosityContextManager(
-                o3d.utility.VerbosityLevel.Debug) as cm:
-            pose_graph = self.full_registration(pcds_down,
-                                           max_correspondence_distance_coarse,
-                                           max_correspondence_distance_fine)
+        args = parser.parse_args()
 
-        print("Optimizing PoseGraph ...")
-        option = o3d.pipelines.registration.GlobalOptimizationOption(
-            max_correspondence_distance=max_correspondence_distance_fine,
-            edge_prune_threshold=0.25,
-            reference_node=0)
-        with o3d.utility.VerbosityContextManager(
-                o3d.utility.VerbosityLevel.Debug) as cm:
-            o3d.pipelines.registration.global_optimization(
-                pose_graph,
-                o3d.pipelines.registration.GlobalOptimizationLevenbergMarquardt(),
-                o3d.pipelines.registration.GlobalOptimizationConvergenceCriteria(),
-                option)
+        voxel_size = args.voxel_size
+        distance_threshold = args.distance_multiplier * voxel_size
+        print("Distance threshold: ", distance_threshold)
 
-        print("Transform points and display")
-        for point_id in range(len(pcds_down)):
-            print(pose_graph.nodes[point_id].pose)
-            pcds_down[point_id].transform(pose_graph.nodes[point_id].pose)
-        o3d.visualization.draw(pcds_down)
-        # --------------------Multiway REGISTRATION END
+        o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Debug)
+        # print('Reading inputs')
+        # src = source
+        # dst = target
+
+        print('Down sampling inputs')
+        src_down, src_fpfh = self.preprocess_point_cloud(sour, voxel_size)
+        dst_down, dst_fpfh = self.preprocess_point_cloud(targ, voxel_size)
+
+        start = time.time()
+        print('RANSAC Started', time.localtime(start))
+        print('Running RANSAC')
+        result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+            src_down, dst_down, src_fpfh, dst_fpfh,
+            mutual_filter=args.mutual_filter,
+            max_correspondence_distance=distance_threshold,
+            estimation_method=o3d.pipelines.registration.
+            TransformationEstimationPointToPoint(True),
+            ransac_n=3,
+            checkers=[
+                o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
+                    0.9),
+                o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                    distance_threshold)
+            ],
+            criteria=o3d.pipelines.registration.RANSACConvergenceCriteria(
+                args.max_iterations, args.confidence))
+        print('RANSAC Finished', time.localtime(time.time()),
+              "Global registration took %.3f sec.\n" % (time.time() - start))
+
+        # sour.paint_uniform_color([1, 0, 0])
+        # targ.paint_uniform_color([0, 1, 0])
+        o3d.visualization.draw([sour.transform(result.transformation), targ])
+
+        # result ICP Local refinement
+        distance_threshold = voxel_size * 0.4
+        print(":: Point-to-plane ICP registration is applied on original point")
+        print("   clouds to refine the alignment. This time we use a strict")
+        print("   distance threshold %.3f." % distance_threshold)
+        result_icp_refined = o3d.pipelines.registration.registration_icp(
+            sour, targ, distance_threshold, result.transformation)
+        print(result_icp_refined)
+        o3d.visualization.draw([sour.transform(result_icp_refined.transformation), targ])
+        # --------------------RANSAC REGISTRATION END
+
+        # # Multiway REGISTRATION START--------------------
+        # voxel_size = 0.02
+        # pcds_down = []
+        # source_down = sour.voxel_down_sample(voxel_size=voxel_size)
+        # pcds_down.append(source_down)
+        # target_down = targ.voxel_down_sample(voxel_size=voxel_size)
+        # pcds_down.append(target_down)
+        # print("Full registration ...")
+        # max_correspondence_distance_coarse = voxel_size * 15
+        # max_correspondence_distance_fine = voxel_size * 1.5
+        # with o3d.utility.VerbosityContextManager(
+        #         o3d.utility.VerbosityLevel.Debug) as cm:
+        #     pose_graph = self.full_registration(pcds_down,
+        #                                    max_correspondence_distance_coarse,
+        #                                    max_correspondence_distance_fine)
+        #
+        # print("Optimizing PoseGraph ...")
+        # option = o3d.pipelines.registration.GlobalOptimizationOption(
+        #     max_correspondence_distance=max_correspondence_distance_fine,
+        #     edge_prune_threshold=0.25,
+        #     reference_node=0)
+        # with o3d.utility.VerbosityContextManager(
+        #         o3d.utility.VerbosityLevel.Debug) as cm:
+        #     o3d.pipelines.registration.global_optimization(
+        #         pose_graph,
+        #         o3d.pipelines.registration.GlobalOptimizationLevenbergMarquardt(),
+        #         o3d.pipelines.registration.GlobalOptimizationConvergenceCriteria(),
+        #         option)
+        #
+        # print("Transform points and display")
+        # for point_id in range(len(pcds_down)):
+        #     print(pose_graph.nodes[point_id].pose)
+        #     pcds_down[point_id].transform(pose_graph.nodes[point_id].pose)
+        # o3d.visualization.draw(pcds_down)
+        # # --------------------Multiway REGISTRATION END
 
     def pick_points(self, pcd):
         print("")
